402bc1d033d3ffeab6b0e4616dcd3ba79870b074
de-fer <dm.chuenkov@gmail.com>
2025-03-08 16:34:18 +0300

    week4

---
+++ Model.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <SDL3/SDL.h>
+
+class Model
+{
+    SDL_Window *window = nullptr;
+    SDL_Renderer *renderer = nullptr;
+
+public:
+    Model();
+    ~Model();
+
+    SDL_AppResult init();
+    SDL_AppResult onEvent(SDL_Event *event);
+    SDL_AppResult iterate();
+
+protected:
+    void clearWindow();
+};
