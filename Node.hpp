97bedccbae7def3c3bb0a5b4120614b539bdf280
de_fer <dm.chuenkov@gmail.com>
2025-03-08 19:59:24 +0300

    Удаление старых файлов
    - Удалены Node.hpp и Texture.hpp (тк реализация будет другой)
    - cmakelists отредактирован соотвествующим образом

--- Node.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <SDL3/SDL.h>
-
-struct Node
-{
-    SDL_FPoint position;
-    double angle;
-};
--- Texture.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <SDL3/SDL.h>
-
-struct Texture
-{
-    SDL_Texture *texture;
-    SDL_FPoint center, size;
-};
