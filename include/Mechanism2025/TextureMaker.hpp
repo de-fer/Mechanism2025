be5914257eb0787a44aee85cac3eff3505af0272
de_fer <dm.chuenkov@gmail.com>
2025-03-10 21:01:54 +0300

    Ошибка загрузки текстуры
    При в случае если не удасться загрузить текстуру, будет сгенерированна текстура ошибки (чёрно-розовый квадрат 2х2)

--- include/Mechanism2025/TextureMaker.hpp
+++ include/Mechanism2025/TextureMaker.hpp
@@ -3,18 +3,6 @@
 #include <SDL3/SDL.h>
 #include <SDL3_image/SDL_image.h>
 
-static SDL_Texture* textureLoader(SDL_Renderer* renderer, const char *file_name)
-{
-    SDL_Texture* texture = IMG_LoadTexture(renderer, file_name);
-    if (!texture){
-        SDL_Log("[TextureMaker::textureLoader] The %s texture wasn't loaded", file_name);
-    }
-    else {
-        SDL_Log("[TextureMaker::textureLoader] The %s texture was loaded as %p", file_name, texture);
-    }
-    return texture;
-}
-
 static SDL_Texture* textureCreateFromSurface(SDL_Renderer* renderer, SDL_Surface* surface)
 {
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
@@ -26,5 +14,52 @@ static SDL_Texture* textureCreateFromSurface(SDL_Renderer* renderer, SDL_Surface
     }
     SDL_DestroySurface(surface);
 
+    SDL_SetTextureScaleMode(texture, SDL_SCALEMODE_NEAREST);
+    return texture;
+}
+
+static SDL_Surface* createErrorSurface()
+{
+    SDL_Surface* surface = SDL_CreateSurface(
+        2, 2
+        , SDL_PIXELFORMAT_RGBA32
+        );
+    SDL_Renderer* renderer = SDL_CreateSoftwareRenderer(surface);
+
+    SDL_SetRenderDrawColorFloat(
+        renderer
+        , 0.f, 0.f, 0.f
+        , SDL_ALPHA_OPAQUE_FLOAT
+        );
+    SDL_RenderPoint(renderer, 0.f,0.f);
+    SDL_RenderPoint(renderer, 1.f,1.f);
+
+    SDL_SetRenderDrawColorFloat(
+        renderer
+        , 1.f, 0.f, 1.f
+        , SDL_ALPHA_OPAQUE_FLOAT
+        );
+    SDL_RenderPoint(renderer, 1.f,0.f);
+    SDL_RenderPoint(renderer, 0.f,1.f);
+    SDL_RenderPresent(renderer);
+
+    SDL_Log("[TextureMaker::createErrorSurface] Surface created: %p", surface);
+
+    SDL_DestroyRenderer(renderer);
+    return surface;
+}
+
+static SDL_Texture* textureLoader(SDL_Renderer* renderer, const char *file_name)
+{
+    SDL_Texture* texture = IMG_LoadTexture(renderer, file_name);
+    if (!texture){
+        SDL_Log("[TextureMaker::textureLoader] The %s texture wasn't loaded. Creating error texture!", file_name);
+        texture = textureCreateFromSurface(renderer, createErrorSurface());
+    }
+    else {
+        SDL_Log("[TextureMaker::textureLoader] The %s texture was loaded as %p", file_name, texture);
+    }
+
+    SDL_SetTextureScaleMode(texture, SDL_SCALEMODE_NEAREST);
     return texture;
 }
